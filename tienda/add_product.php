22b6e65e8c868a7fb6f28776268a05d5f02f1465
TurpialCode <84598876+PFT-Repo@users.noreply.github.com>
2022-11-21 13:51:26 +0100

    ready product.php

--- tienda/add_product.php
+++ tienda/add_product.php
@@ -1,29 +1,25 @@
 <?php include("_header.php"); ?>
 	<?php 
 		if(isset($_POST['send_contact'])){
-			$name = $_POST['name'];
-			echo ($name);
+			print_r($_POST);
+			$nombre = $_POST['nombre'];
 			$price = $_POST['price'];
-			echo ($price);
 			$category_id = $_POST['category_id'];
-			echo ($category_id);
 			$descripcion = $_POST['descripcion'];
-			echo ($descripcion);
 			$url_foto = $_POST['url_foto'];
-			echo ($url_foto);
 			$n_opiniones = $_POST['n_opiniones'];
-			echo ($n_opiniones);
 
 			if(isset($nombre) && isset($price) && isset($category_id) && isset($descripcion) && isset($url_foto) && isset($n_opiniones)){
-				echo "Guardar producto";
-				$sql = "INSERT INTO productos ('categoria_id', 'nombre', 'descripcion','precio','url_foto','n_opiniones') VALUES (('".$category_id."'),('".$name."'),('".$descripcion."'),('".$price."'),('".$url_foto."'),('".$n_opiniones."'));";
-			if(mysqli_query($mysqli, $sql)) {
-				//Tot correcte
-				$okey = "Añadido correctamente";
-			}else{
-				//Si és produeix un error
-				$error = "Error: " . $sql . "<br>" . mysqli_error($mysqli);
-			}
+				
+				$sql = "INSERT INTO productos (categoria_id,nombre,descripcion,precio,url_foto,n_opiniones) VALUES (".$category_id.",'".$nombre."','".$descripcion."',".$price.",'".$url_foto."',".$n_opiniones.")";
+				echo $sql;
+				if(mysqli_query($mysqli, $sql)) {
+					//Tot correcte
+					$okey = "Añadido correctamente";
+				}else{
+					//Si és produeix un error
+					$error = "Error: " . $sql . "<br>" . mysqli_error($mysqli);
+				}
 
 			}else{
 				echo "Faltan datos";
@@ -42,16 +38,28 @@
 	
 			<!-- Name input-->
 			<div class="row mb-3">
-			  <label class="col-md-3 control-label" for="name">Nombre</label>
+			  <label class="col-md-3 control-label" for="nombre">Nombre</label>
 			  <div class="col-md-9">
-				<input id="name" name="name" type="text" placeholder="calcetin" class="form-control" required>
+				<input id="nombre" name="nombre" type="text" placeholder="calcetin" class="form-control" required>
 			  </div>
 			</div>
 
 			<div class="row mb-3">
 			  <label class="col-md-3 control-label" for="category_id">Número de categoria</label>
 			  <div class="col-md-9">
-				<input id="category_id" name="category_id" type="number" placeholder="1-5" class="form-control" required>
+			  <select name="category_id" class="form-select" required>
+				  <option value=""></option>				
+				  <?php
+				  $sql_cat = 'SELECT * FROM categorias';
+				  $resultado_cat = mysqli_query($mysqli, $sql_cat);
+				  while ($row_cat = mysqli_fetch_assoc($resultado_cat)):
+					?>	
+						<option value="<?= $row_cat['id_categoria'] ?>"><?= $row_cat['nombre'] ?></option>
+				<?php
+					endwhile;
+		
+				?>
+				</select>
 			  </div>
 			</div>
 	
